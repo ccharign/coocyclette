7a516bad70e39641c7fda613fbf68a42a2029459
cyril charignon <cyril.charignon@net-c.com>
2023-10-26 17:18:14 +0200

    épaisseur et opacité des trajets

--- src/classes/Itinéraire.ts
+++ src/classes/Itinéraire.ts
@@ -11,17 +11,22 @@ export class Itinéraire {
     longueur: number // en km
     polyline: L.Polyline
 
-    constructor({ points, couleur,  pourcentage_détour, longueur, nom }: GetItinéraire) {
+    constructor({ points, couleur, pourcentage_détour, longueur, nom }: GetItinéraire) {
 
         this.couleur = couleur;
         this.longueur = longueur;
         this.pourcentage_détour = pourcentage_détour;
         const contenu_popup = `${nom}<br>${longueur}km, ${Math.floor(longueur / 0.25)}mn`
-            +( pourcentage_détour ? `,<br> détour de ${pourcentage_détour}%` : "");
+            + (pourcentage_détour ? `,<br> détour de ${pourcentage_détour}%` : "");
 
         this.polyline = new L.Polyline(
             géomOsmVersLeaflet(points),
-            { color: couleur }
+            {
+                color: couleur,
+                weight: 7,
+                opacity: .6,
+                lineJoin: "miter",
+            }
         )
             .bindPopup(contenu_popup);
 
