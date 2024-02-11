15b034787b836b13cb61226bfd1b64c0274f6b29
cyril charignon <cyril.charignon@net-c.com>
2024-02-11 11:34:15 +0100

    alerte si hors zone

--- src/composants/molécules/BoutonEnvoi.tsx
+++ src/composants/molécules/BoutonEnvoi.tsx
@@ -22,11 +22,11 @@ type propsBoutonEnvoi = {
 
 export default function BoutonEnvoi({ texte, disabled }: propsBoutonEnvoi) {
 
-    
+
     const { zone, setTiroir, carte, itinéraires, étapes, setStats } = useContext(contexte_iti);
     const [iti_en_chargement, setItiEnChargement] = useState(false);
 
-    
+
     // màj la liste de toutes les étapes via setToutesLesÉtapes
     // et lance la recherche d’itinéraires
     async function envoieForm() {
@@ -37,11 +37,16 @@ export default function BoutonEnvoi({ texte, disabled }: propsBoutonEnvoi) {
             const étapes_django = étapes.toutes_les_étapes().map(é => é.pourDjango());
             const url = new URL(`${URL_API}itineraire/${zone}`);
             url.searchParams.append("étapes_str", JSON.stringify(étapes_django));
-            const res = await (fetch(url).then(res => res.json())) as GetItinéraire[];
-            màjItinéraires(res, carte as L.Map, itinéraires, étapes, setStats);
-            //setDonnéesModifiées(false);
-            setTiroir("contribuer", true);
-            
+            const réponse_serveur = await fetch(url);
+            if (réponse_serveur.status === 500) {
+                const message = (await réponse_serveur.json()).message;
+                alert("Erreur côté serveur : " + message)
+            } else {
+                const res = await (fetch(url).then(res => res.json())) as GetItinéraire[];
+                màjItinéraires(res, carte as L.Map, itinéraires, étapes, setStats);
+                //setDonnéesModifiées(false);
+                setTiroir("contribuer", true);
+            }
 
         } catch (error) {
             console.log(error);
